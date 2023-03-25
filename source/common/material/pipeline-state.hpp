c434714917545b49d271c6d099e20353c6f98763
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2023-03-25 18:28:51 +0200

    Verfied ✅✅

--- source/common/material/pipeline-state.hpp
+++ source/common/material/pipeline-state.hpp
@@ -44,38 +44,44 @@ namespace our
 
         // This function should set the OpenGL options to the values specified by this structure
         // For example, if faceCulling.enabled is true, you should call glEnable(GL_CULL_FACE), otherwise, you should call glDisable(GL_CULL_FACE)
-        void setup() const {
-            //TODO: (Req 4) Write this function
-            if(faceCulling.enabled){
+        void setup() const
+        {
+            // TODO: (Req 4) Write this function
+            if (faceCulling.enabled)
+            {
                 glEnable(GL_CULL_FACE);
                 glCullFace(faceCulling.culledFace);
                 glFrontFace(faceCulling.frontFace);
             }
-            else{
+            else
+            {
                 glDisable(GL_CULL_FACE);
             }
 
-            if(depthTesting.enabled){
+            if (depthTesting.enabled)
+            {
                 glEnable(GL_DEPTH_TEST);
                 glDepthFunc(depthTesting.function);
             }
-            else{
+            else
+            {
                 glDisable(GL_DEPTH_TEST);
             }
 
-            if(blending.enabled){
+            if (blending.enabled)
+            {
                 glEnable(GL_BLEND);
                 glBlendEquation(blending.equation);
                 glBlendFunc(blending.sourceFactor, blending.destinationFactor);
                 glBlendColor(blending.constantColor.r, blending.constantColor.g, blending.constantColor.b, blending.constantColor.a);
             }
-            else{
+            else
+            {
                 glDisable(GL_BLEND);
             }
 
             glColorMask(colorMask.r, colorMask.g, colorMask.b, colorMask.a);
             glDepthMask(depthMask);
-            
         }
 
         // Given a json object, this function deserializes a PipelineState structure
