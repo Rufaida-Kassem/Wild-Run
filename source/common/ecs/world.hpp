fa91331d3ce1ebdf921fd0dd9550f5da6af24acd
Doaa Magdy <71094316+doaamagdy2024@users.noreply.github.com>
2023-04-09 03:26:50 +0200

    world.hpp completed

--- source/common/ecs/world.hpp
+++ source/common/ecs/world.hpp
@@ -41,17 +41,30 @@ namespace our {
         // The elements in the "markedForRemoval" set will be removed and deleted when "deleteMarkedEntities" is called.
         void markForRemoval(Entity* entity){
             //TODO: (Req 8) If the entity is in this world, add it to the "markedForRemoval" set.
+            if (entities.find(entity) != entities.end())
+            {
+                markedForRemoval.insert(entity);
+            }
         }
 
         // This removes the elements in "markedForRemoval" from the "entities" set.
         // Then each of these elements are deleted.
         void deleteMarkedEntities(){
             //TODO: (Req 8) Remove and delete all the entities that have been marked for removal
+            for (auto entity : markedForRemoval)
+            {
+                entities.erase(entity);
+                delete entity;
+            }
         }
 
         //This deletes all entities in the world
         void clear(){
             //TODO: (Req 8) Delete all the entites and make sure that the containers are empty
+            for (auto entity : entities)
+            {
+                delete entity;
+            }
             
         }
 
