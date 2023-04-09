614ab41a18255f4e83734dd131ef5ea3c563ed3a
Doaa Magdy <71094316+doaamagdy2024@users.noreply.github.com>
2023-04-09 03:18:21 +0200

    entity.hpp class is completed

--- source/common/ecs/entity.hpp
+++ source/common/ecs/entity.hpp
@@ -38,7 +38,10 @@ namespace our
             static_assert(std::is_base_of<Component, T>::value, "T must inherit from Component");
             // TODO: (Req 8) Create an component of type T, set its "owner" to be this entity, then push it into the component's list
             //  Don't forget to return a pointer to the new component
-            return nullptr;
+            T *comp = new T();
+            comp->owner = this;
+            components.push_back(comp);
+            return comp;
         }
 
         // This template method searhes for a component of type T and returns a pointer to it
@@ -49,6 +52,12 @@ namespace our
         {
             // TODO: (Req 8) Go through the components list and find the first component that can be dynamically cast to "T*".
             //  Return the component you found, or return null of nothing was found.
+            for (auto it = components.begin(); it != components.end(); it++)
+            {
+                T *comp = dynamic_cast<T *>(*it);
+                if (comp != nullptr)
+                    return comp;
+            }
             return nullptr;
         }
 
@@ -70,6 +79,16 @@ namespace our
         {
             // TODO: (Req 8) Go through the components list and find the first component that can be dynamically cast to "T*".
             //  If found, delete the found component and remove it from the components list
+            for (auto it = components.begin(); it != components.end(); it++)
+            {
+                T *comp = dynamic_cast<T *>(*it);
+                if (comp != nullptr)
+                {
+                    delete comp;
+                    components.erase(it);
+                    break;
+                }
+            }
         }
 
         // This template method searhes for a component of type T and deletes it
@@ -90,12 +109,25 @@ namespace our
         {
             // TODO: (Req 8) Go through the components list and find the given component "component".
             //  If found, delete the found component and remove it from the components list
+            for (auto it = components.begin(); it != components.end(); it++)
+            {
+                if (*it == component)
+                {
+                    delete *it;
+                    components.erase(it);
+                    break;
+                }
+            }
         }
 
         // Since the entity owns its components, they should be deleted alongside the entity
         ~Entity()
         {
             // TODO: (Req 8) Delete all the components in "components".
+            for (auto it = components.begin(); it != components.end(); it++)
+            {
+                delete *it;
+            }
         }
 
         // Entities should not be copyable
