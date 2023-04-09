c84de2c3c0640c85b17d9b35e5aa23ec2c8ed185
Doaa Magdy <71094316+doaamagdy2024@users.noreply.github.com>
2023-04-09 04:37:12 +0200

    camera.cpp completed

--- source/common/components/camera.cpp
+++ source/common/components/camera.cpp
@@ -35,7 +35,14 @@ namespace our {
         // - the center position which is the point (0,0,-1) but after being transformed by M
         // - the up direction which is the vector (0,1,0) but after being transformed by M
         // then you can use glm::lookAt
-        return glm::mat4(1.0f);
+        
+        //determine the position of the camera
+        glm::mat4 view = glm::lookAt(
+            glm::vec3(M * glm::vec4(0, 0, 0, 1)), //position of the camera  --> rotating
+            glm::vec3(M * glm::vec4(0, 0, -1.0, 1)), //position of the object --> let camera look at the center (as the obj exist at origin)
+            glm::vec3(M * glm::vec4(0, 1.0, 0, 1) ) //up vector
+        );
+        return view;
     }
 
     // Creates and returns the camera projection matrix
@@ -46,6 +53,13 @@ namespace our {
         // It takes left, right, bottom, top. Bottom is -orthoHeight/2 and Top is orthoHeight/2.
         // Left and Right are the same but after being multiplied by the aspect ratio
         // For the perspective camera, you can use glm::perspective
-        return glm::mat4(1.0f);
+        glm::mat4 projection;
+        if(cameraType == CameraType::PERSPECTIVE){
+            projection = glm::perspective(fovY, float(viewportSize.x) / viewportSize.y, near, far);
+        }
+        else if(cameraType == CameraType::ORTHOGRAPHIC){
+            projection = glm::ortho(-orthoHeight * (viewportSize.x / viewportSize.y), orthoHeight * (viewportSize.x / viewportSize.y), -orthoHeight / 2, orthoHeight / 2, near, far);
+        }
+        return projection;
     }
 }
