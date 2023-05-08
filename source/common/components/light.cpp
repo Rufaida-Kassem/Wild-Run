c5f8cc194b800e1dd54676d4a94bccd70e1f533b
rufaida-kassem <rufaidahkassem@gmail.com>
2023-05-08 20:08:49 +0300

    light component

---
+++ source/common/components/light.cpp
@@ -0,0 +1,56 @@
+#include "light.hpp"
+#include "../ecs/entity.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace our
+{
+    //. Reads light parameters from the given json object
+    void LightComponent::deserialize(const nlohmann::json &data)
+    {
+        if (!data.is_object())
+            return;
+        std::string lightTypeStr = data.value("lightType", "directional");
+
+        if (lightTypeStr == "directional")
+        {
+            lightType = LightType::DIRECTIONAL;
+        }
+        else if (lightTypeStr == "point")
+        {
+            lightType = LightType::POINT;
+        }
+        else
+        {
+            lightType = LightType::SPOT;
+        }
+
+        //. same for all light types
+        color = glm::vec4(data.value("color", glm::vec3(1.0f)), 1.0f);
+        intensity = data.value("intensity", 1.0f);
+        diffuse = data.value("diffuse", glm::vec3(1.0f));
+        specular = data.value("specular", glm::vec3(1.0f));
+
+        //. for directional light, the ambient coefficient is always 1
+        //. to decide later if we want to change it
+
+        if (lightType == LightType::DIRECTIONAL)
+        {
+            ambient = glm::vec3(1.0f);
+        }
+        else
+        {
+            ambient = data.value("ambient", glm::vec3(0.0f));
+            attenuation = data.value("attenuation", glm::vec3(1.0f, 0.0f, 0.0f));
+
+        }
+        
+        //. to get the cone angles, we need to convert from degrees to radians
+        //. the default values can be changed later
+        if (lightType == LightType::SPOT)
+        {
+            cone_angles = glm::radians(data.value("cone_angles", glm::vec2(30.0f, 45.0f)));
+        }
+
+    }
+}
---
+++ source/common/components/light.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "../ecs/component.hpp"
+
+#include <glm/mat4x4.hpp>
+
+namespace our
+{
+
+    // An enum that defines the type of the light (DIRECTIONAL, POINT or SPOT)
+    enum class LightType
+    {
+        DIRECTIONAL,
+        POINT,
+        SPOT
+    };
+
+    class LightComponent : public Component
+    {
+    public:
+        LightType lightType; //. The type of the light
+        glm::vec4 color;     //. The color of the light
+        float intensity;     //. The intensity of the light
+        glm::vec3 diffuse;   //. The diffuse coefficient of the light
+        glm::vec3 specular;  //. The specular coefficient of the light
+        glm::vec3 ambient;   //. The ambient coefficient of the light
+        glm::vec3 attenuation; //. x*d^2 + y*d + z
+        glm::vec2 cone_angles; //. x: inner_angle, y: outer_angle
+        //. Reads light parameters from the given json object
+        void deserialize(const nlohmann::json& data) override;
+        //. identify it is a light component
+        static std::string getID() { return "light"; }
+    };
+
+}
