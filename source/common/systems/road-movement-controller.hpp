b79d99adff37f8d940b0e3d52782898ac583f1c1
Doaa Magdy <71094316+doaamagdy2024@users.noreply.github.com>
2023-05-19 16:06:26 +0300

    documentation for obstalce-controller

--- source/common/systems/road-movement-controller.hpp
+++ source/common/systems/road-movement-controller.hpp
@@ -33,18 +33,6 @@ namespace our
 		// if so then make it in front of the seocond road
 		// i.e., let its z equals to the second road z + its length
 		// we will check if the road is behind the camera every frame
-		// but should we check for the z of road to be greater or less than the camera z?
-		// if we check for the z of road to be greater than the camera z
-		// then the road will be in front of the camera
-		// but if we check for the z of road to be less than the camera z
-		// then the road will be behind the camera
-		// so we will check for the z of road to be less than the camera z
-		// but should we compare it by the camera z or the camera z - the length of the road?
-		// if we compare it by the camera z
-		// then the road will be behind the camera
-		// but if we compare it by the camera z - the length of the road
-		// then the road will be in front of the camera
-
 
 
 		// This should be called every frame to update all entities containing a FreeCameraControllerComponent 
@@ -66,7 +54,7 @@ namespace our
 					controller = entity->getComponent<FreeCameraControllerComponent>();
 				if (road1 && road2 && controller) break;
 			}
-			// If there is no entity with both a CameraComponent and a FreeCameraControllerComponent, we can do nothing so we return
+			// if there is no neither road nor controller, then return
 			if (!(road1 && road2 && controller)) return;
 			// Get the entity that we found via getOwner of camera (we could use controller->getOwner())
 			Entity* entity1 = road1->getOwner();
@@ -76,13 +64,13 @@ namespace our
             glm::vec3& camera_position = controller->getOwner()->localTransform.position;
 
 
-			// We get a reference to the entity's position and rotation
+			// We get a reference to the entity's position for each road
 			glm::vec3& position1 = entity1->localTransform.position;
 			glm::vec3& position2 = entity2->localTransform.position;
 
-			// check if the road z position is behind the camera
-            // if so then make it in front of the seocond road
-            // i.e., let its z equals to the second road z + its length
+			// check if the the end of the road becomes behind the camera
+            // then make it in front of the seocond road
+            // i.e., let its z equals to the second road z - (legnth of road1 + lengnth of road2)
 			if(position1.z < position2.z)
 			{
 				if (position2 .z >= camera_position.z + 30) // 20 is the length of the road (scalling in y)
