2805e6dc44b1532da17b0946f67ad372f97d2878
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2023-05-22 00:28:51 +0300

    Docs

--- source/states/menu-state.hpp
+++ source/states/menu-state.hpp
@@ -13,42 +13,49 @@
 #include "systems/movement.hpp"
 #include "systems/preview-camera-controller.hpp"
 
-
 // This state shows how to use some of the abstractions we created to make a menu.
-class Menustate : public our::State {
-
+class Menustate : public our::State
+{
 
     float time;
     // to render a background image
     our::ForwardRenderer renderer;
     our::World world;
-    our::MovementSystem movementSystem;
-
+    // to move the objects
+    // our::MovementSystem movementSystem;
+    // to control the preview camera
     our::PreviewCameraControllerSystem previewController;
 
-    void onInitialize() override {
+    void onInitialize() override
+    {
         // First of all, we get the scene configuration from the app config
         auto &config = getApp()->getConfig()["menu-scene"];
         // If we have assets in the scene config, we deserialize them
-        if (config.contains("assets")) {
+        if (config.contains("assets"))
+        {
             our::deserializeAllAssets(config["assets"]);
         }
         // If we have a world in the scene config, we use it to populate our world
-        if (config.contains("world")) {
+        if (config.contains("world"))
+        {
             world.deserialize(config["world"]);
         }
         // We initialize the camera controller system since it needs a pointer to the app
         // Then we initialize the renderer
         auto size = getApp()->getFrameBufferSize();
+        // We initialize the renderer with a size of 3/4 of the window size height
         renderer.initialize({size.x, size.y * 3 / 4}, config["renderer"]);
+        // We initialize the preview controller system
         previewController.enter(getApp(), &world);
+        // We deserialize the players models from the config
         previewController.deserializePlayers(config["players-entities"]);
     }
 
-    void onDraw(double deltaTime) override {
+    void onDraw(double deltaTime) override
+    {
         // call the update function of the preview controller and the movement system to update the world
-        previewController.update((float) deltaTime);
-        movementSystem.update(&world, (float) deltaTime);
+        previewController.update((float)deltaTime);
+        // movementSystem.update(&world, (float)deltaTime);
         // Delete all the entities that are marked for deletion
         world.deleteMarkedEntities();
         // Render the world using the renderer
@@ -56,20 +63,23 @@ class Menustate : public our::State {
         // Get a reference to the keyboard object
         auto &keyboard = getApp()->getKeyboard();
 
-        if (keyboard.justPressed(GLFW_KEY_SPACE)) {
+        if (keyboard.justPressed(GLFW_KEY_SPACE))
+        {
             // If the space key is pressed in this frame, go to the play state
             getApp()->changeState("play");
         }
-        if (keyboard.justPressed(GLFW_KEY_ESCAPE)) {
+        if (keyboard.justPressed(GLFW_KEY_ESCAPE))
+        {
             // If the escape key is pressed in this frame, quit the application
             getApp()->close();
         }
     }
 
-    int i = 0;
+    int player_index = 0;
 
-    void onImmediateGui() {
-        //        ImGui::ShowDemoWindow();
+    void onImmediateGui()
+    {
+        // some flags to make the window transparent and not movable
         ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDecoration |
                                         ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize |
                                         ImGuiWindowFlags_NoSavedSettings |
@@ -79,20 +89,18 @@ class Menustate : public our::State {
 
         ImGui::SetWindowFontScale(2);
         ImGui::Text("Welcome To The Game");
-
         ImGui::Text("Press space to Play");
         ImGui::Text("Press escape to Quit");
         ImGui::Text("Left and Right arrows to choose the player");
-        //        ImGui::PopFont();
         ImGui::End();
-
+        // write the player index in the window
         ImGui::Begin("Players", nullptr, window_flags);
         ImGui::SetCursorPos(ImVec2(110, 222));
         ImGui::Text("Player %d", previewController.getCurrentPlayer());
 
-        //        vertical slider to choose the player
+        // vertical slider to choose the player
         glm::vec2 window_size = getApp()->getFrameBufferSize();
-        i = previewController.getCurrentPlayer();
+        player_index = previewController.getCurrentPlayer();
         // Save the current style
         ImGuiStyle &style = ImGui::GetStyle();
         ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(8, 8)); // Increase padding
@@ -104,21 +112,23 @@ class Menustate : public our::State {
 
         // Display the slider
         ImGui::SetNextWindowPos(ImVec2(ImGui::GetCursorScreenPos().x, ImGui::GetCursorScreenPos().y + 18));
-        ImGui::VSliderInt("##v", ImVec2(18, 160), &i, 0, previewController.getPlayerCount() - 1);
+        ImGui::VSliderInt("##v", ImVec2(18, 160), &player_index, 0, previewController.getPlayerCount() - 1);
 
         // Restore the previous style
         ImGui::PopStyleVar(2);
         ImGui::PopStyleColor(2);
 
-        if (i != previewController.getCurrentPlayer()) {
-            previewController.changePlayer(i);
+        if (player_index != previewController.getCurrentPlayer())
+        {
+            previewController.changePlayer(player_index);
         }
         ImGui::End();
 
         //        imgui tabs to show thee chosen player index from the players
     }
 
-    void onDestroy() override {
+    void onDestroy() override
+    {
         // Delete all the allocated resources
         world.clear();
         renderer.destroy();
