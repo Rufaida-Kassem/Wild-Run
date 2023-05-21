467f2bbdf6c617edec5d462b107e7d7d2e238650
Doaa Magdy <71094316+doaamagdy2024@users.noreply.github.com>
2023-05-21 21:52:03 +0300

    documentation for lightpole.hpp

--- source/common/components/lightpole.hpp
+++ source/common/components/lightpole.hpp
@@ -10,7 +10,9 @@ namespace our {
     // we need this component so that we can apply operation on it such as redrawing
     class LightPoleComponent : public Component {
     public:
-        // The ID of this component type is "Obstacle"
+        // The ID of this component type is "lightpole"
+        // this function is used to get the id of the component
+        // so that we can deserialize it and insert it in the world entities
         static std::string getID() { return "lightpole"; }
 
         // this is the decleratoin of the deserialize function
