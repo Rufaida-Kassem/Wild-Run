8d2b40a01004975af7d49c7044a136594bd2eb1d
Doaa Magdy <71094316+doaamagdy2024@users.noreply.github.com>
2023-05-21 19:06:49 +0300

    documentation for lightpole.hpp

--- source/common/components/lightpole.hpp
+++ source/common/components/lightpole.hpp
@@ -7,13 +7,13 @@
 namespace our {
 
     // this is a light pole component
-    // we need this component so that we can apply operation on it such as redrawing,
-    // exist form the game once a player hits it
+    // we need this component so that we can apply operation on it such as redrawing
     class LightPoleComponent : public Component {
     public:
         // The ID of this component type is "Obstacle"
         static std::string getID() { return "lightpole"; }
 
+        // this is the decleratoin of the deserialize function
         void deserialize(const nlohmann::json& data) override;
     };
 
