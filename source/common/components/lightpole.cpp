7d8d2be5c57836e973a74a367af6a5bf486a290d
Doaa Magdy <71094316+doaamagdy2024@users.noreply.github.com>
2023-05-21 21:51:38 +0300

    documentation for kugtoike,coo

--- source/common/components/lightpole.cpp
+++ source/common/components/lightpole.cpp
@@ -4,10 +4,11 @@
 
 namespace our {
     
-    // the follwoing function is overriden from the component class
-    // it is used to deserialize the lightpole from the josn file
-    // it takes the json object as a parameter
-    // to deserialize the lightpole from the json file, the desrialize function of
+    // the follwoing function is overriden from the component class 
+    // cause the lightpole component inherits from the component class 
+    // and this function is virtual in the component class 
+    // and hence it must be overriden or else the light pole component will be an abstract class
+    // but actually the light pole component is deserialized by the entity class after inserting it into the world
     void LightPoleComponent::deserialize(const nlohmann::json& data){
         if(!data.is_object()) return;
         
